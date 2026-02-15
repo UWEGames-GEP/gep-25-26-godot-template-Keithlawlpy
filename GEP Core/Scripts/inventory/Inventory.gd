3422fc9697a7d57ecb71d6b0f95a99506dcb7a6e
Keithlawlpy <164499416+Keithlawlpy@users.noreply.github.com>
2026-02-15 15:55:18 +0000

    player can pick up object now

--- GEP Core/Scripts/inventory/Inventory.gd
+++ GEP Core/Scripts/inventory/Inventory.gd
@@ -4,10 +4,14 @@ extends Node3D
 @export var game_manager: Node
 
 # ---- Inventory ----
-var items: Array = []
+var items: Array[String] = []
 
 # ---- World Items Parent ----
 var world_items: Node3D
 
 func _ready() -> void:
 	game_manager = get_tree().get_first_node_in_group("game_manager")
+
+func add_item(id: String):
+	items.append(id)
+	print("Picked up: ", id)
--- GEP Core/Scripts/inventory/ItemObject.gd
+++ GEP Core/Scripts/inventory/ItemObject.gd
@@ -15,13 +15,18 @@ enum RarityType {
 # ---- Item Info ----
 @export var item_name: String = ""
 
-func _ready() -> void:
-	pass
-
+func _ready():
+	body_entered.connect(_on_body_entered)
+	
 func _process(delta):
 	rotate(Vector3.UP, 1.0 * delta)
-
-
-func _on_body_entered(_body: Node3D) -> void:
-	queue_free()
 	
+	
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		# Access the player's InventoryManager
+		var inv = body.get_node("InventoryManager")
+		inv.add_item(item_name)
+		
+		queue_free()
+		
